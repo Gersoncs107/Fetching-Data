6eacab8ca885b3c838a703f1417fe9ddd5643f47
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-24 23:43:35 -0300

    react

--- src/fetch.jsx
+++ src/fetch.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 const Image = () => {
   const [imageURL, setImageURL] = useState(null);
   const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
