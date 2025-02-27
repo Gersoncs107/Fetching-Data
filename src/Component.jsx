edededf6080809a86d5407ca03ef7ac3fa5b1053
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-27 19:56:38 -0300

    react

--- src/Component.jsx
+++ src/Component.jsx
@@ -4,10 +4,11 @@ const Component = () => {
     const [data, setData] = useState()
 
     useEffect(()=> {
+
         const dataFetch = async ()=> {
              const data = await(
             await fetch('https://run.mocky.io/v3/d6155d63-938f-484c-8d87-6f918f126cd4')
-        )
+        ).json()
         }
        
     })
