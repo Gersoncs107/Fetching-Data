d0875268049725f132fab2dd8845bc67f296df72
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-27 19:55:48 -0300

    react

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,5 +1,14 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const Component = () => {
     const [data, setData] = useState()
+
+    useEffect(()=> {
+        const dataFetch = async ()=> {
+             const data = await(
+            await fetch('https://run.mocky.io/v3/d6155d63-938f-484c-8d87-6f918f126cd4')
+        )
+        }
+       
+    })
 }
