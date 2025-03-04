15ac311e1061606be23e73b29addac88748faeb6
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-03 22:29:17 -0300

    react

--- src/Ref.jsx
+++ src/Ref.jsx
@@ -3,6 +3,10 @@ import { useRef, useEffect } from "react";
 const ButtonComponent = () => {
     const buttonRef = useRef(null)
 
+    useEffect(()=> {
+
+    }, [])
+
     return(
         <button ref={buttonRef}>Click Me!</button>
     )
