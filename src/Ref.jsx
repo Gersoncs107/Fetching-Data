a7d57c5cba8ed0484b2c6fdcc78135c8db8ad6d3
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-04 20:11:10 -0300

    react

--- src/Ref.jsx
+++ src/Ref.jsx
@@ -3,18 +3,18 @@ import { useRef, useEffect } from "react";
 const ButtonComponent = () => {
     const buttonRef = useRef(null)
 
-    useEffect(()=> {
-        buttonRef.current.focus()
-        buttonRef.current.textContent = "Hey, I'm different!"
-        let temeout = setTimeout(()=> {
-            buttonRef.current.textContent = "Click Me!"
-        }, 2000)
-        
+    useEffect(() => {
+        buttonRef.current.focus();
+        buttonRef.current.textContent = "Hey, I'm different!";
+        let timeout = setTimeout(() => {
+          buttonRef.current.textContent = "Click Me!";
+        }, 2000);
+      
         return () => {
-            clearTimeout(timeout)
-        }
-
-    }, [])
+          clearTimeout(timeout);
+        };
+      }, []);
+      
 
     return(
         <button ref={buttonRef}>Click Me!</button>
