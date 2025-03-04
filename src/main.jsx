6f7db072104d270b169c4746f04ac27506cb0ca1
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-03 22:40:07 -0300

    react

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Image from './fetch.jsx'
+import ButtonComponent from './Ref.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Image />
+    <ButtonComponent />
   </StrictMode>,
 )
