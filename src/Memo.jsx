c6c5b2e972a8768b09d440035fc91a7da41b8a10
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-04 22:47:24 -0300

    react

--- src/Memo.jsx
+++ src/Memo.jsx
@@ -3,4 +3,19 @@ import { useState } from "react";
 const ButtonComponent = ({children, onClick}) => {
     let i = 0
     let j= 0
+
+    const ITERATION_COUNT = 10_000
+    while(i < ITERATION_COUNT){
+        while(j < ITERATION_COUNT){
+            j+=1
+        }
+        i+= 1
+        j+= 0
+    }
+    
+    return(
+        <button type="button" onClick={onClick}>
+            {children}
+        </button>
+    )
 }
