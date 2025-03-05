677dd1b31661c277558fc91d1c70583cb4c29658
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-04 22:53:45 -0300

    react

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ButtonComponent from './Ref.jsx'
+import Counter from './Memo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ButtonComponent />
+    <Counter />
   </StrictMode>,
 )
