d3eabcfef7d2a1c0e902913ea715f21dff6d2ac3
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-05 23:12:32 -0300

    react

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,24 +1,21 @@
 import { useState } from "react"
 
-const CountButton = ({onClick, count}) => {
-    return(
-        <button onClick={onClick}>{count}</button>
-    )
-}
-
-const DualCounter = () => {
+function CountButton({onClick, count}) {
+    return <button onClick={onClick}>{count}</button>
+  }
+  
+export default function DualCounter() {
     const [count1, setCount1] = useState(0)
-    const increase = () => setCount1(c => c + 1)
-
+    const increment1 = () => setCount1(c => c + 1)
+  
     const [count2, setCount2] = useState(0)
-    const decrease = () => setCount2(c => c - 1)
-
-    return(
-        <div>
-            <CountButton count={count1} onClick={increase}/>
-            <CountButton count={count2} onClick={decrease}/>
-        </div>
+    const increment2 = () => setCount2(c => c + 1)
+  
+    return (
+      <>
+        <CountButton count={count1} onClick={increment1} />
+        <CountButton count={count2} onClick={increment2} />
+      </>
     )
-}
+  }
 
-export default DualCounter
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import ButtonComponent from './Ref.jsx'
 import Counter from './Memo.jsx'
+import DualCounter from './Count.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter />
+    <DualCounter />
   </StrictMode>,
 )
