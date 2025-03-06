ed77b3b529adf4ae5cdc1bb5dfcd787faa3fb9b5
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-05 23:09:16 -0300

    react

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react"
 
+const CountButton = ({onClick, count}){
+    return(
+        <button onClick={onClick}>{count}</button>
+    )
+}
+
 const DualCounter = () => {
     const [count1, setCount1] = useState(0)
     const increase = () => setCount1(c => c + 1)
 
     const [count2, setCount2] = useState(0)
     const decrease = () => setCount2(c => c - 1)
+
+    return(
+        <div>
+            <CountButton count={count1} onClick={increase}/>
+            <CountButton count={count2} onClick={decrease}/>
+        </div>
+    )
 }
 
