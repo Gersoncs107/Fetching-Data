abc4d1e1124d689c02d8dd36d5b0000b4d604a54
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-05 22:21:51 -0300

    react

--- src/Candy.jsx
+++ src/Candy.jsx
@@ -10,7 +10,21 @@ function CandyDispenser() {
 
     return(
         <div>
-            
+        <h1>Candy Dispenser</h1>
+        <div>
+          <div>Available Candy</div>
+          {candies.length === 0 ? (
+            <button onClick={() => setCandies(initialCandies)}>refill</button>
+          ) : (
+            <ul>
+              {candies.map(candy => (
+                <li key={candy}>
+                  <button onClick={() => dispense(candy)}>grab</button> {candy}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
+      </div>
     )
 }
