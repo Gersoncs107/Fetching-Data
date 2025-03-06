ac083c1a40df3e1ce18b1d0752527706eaa5a909
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-05 22:56:26 -0300

    react

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
 
 const DualCounter = () => {
+    const [count1, setCount1] = useState(0)
+    const increase = () => setCount1(c => c + 1)
 
-
+    const [count2, setCount2] = useState(0)
+    const decrease = () => setCount2(c => c - 1)
 }
+
