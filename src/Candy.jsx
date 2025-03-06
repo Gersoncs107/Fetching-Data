90c6d52be87df244a519d1865fde09608cc252c7
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-05 22:18:54 -0300

    react

--- src/Candy.jsx
+++ src/Candy.jsx
@@ -3,4 +3,8 @@ import { useCallback, useState } from "react";
 function CandyDispenser() {
     const initialCandies = ['snickers', 'skittles', 'twix', 'milky way']
     const [candies, setCandies] = useState(initialCandies)
+
+    const dispense = (candy) => {
+        setCandies(allCandies => allCandies.filter())
+    }
 }
