078f2ca2c5cfc7cbb6840fba8d6ea377769d248c
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-07 22:56:07 -0300

    react

---
+++ src/useRef.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function FormComponent() {
+    const [name, setName] = React.useState('')
+   
+    const handleChange = (e) => {
+      setName(e.target.value)
+    }
