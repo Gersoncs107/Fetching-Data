9cd300d89b0eddcc75345a005d66ffef4562d4da
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-08 19:44:12 -0300

    react

--- src/useRef.jsx
+++ src/useRef.jsx
@@ -29,3 +29,26 @@ export default function FormComponent() {
           />
         </form>
       )}
+
+      const handleSubmit2 = () => {
+        const nameInput = document.querySelector('#name').value
+     
+        // checks if the input field is empty.
+        // if it is an alert is fired.
+        if (nameInput === '') {
+          alert('name cannot be blank')
+        }
+      }
+
+      return (
+        <form onSubmit={handleSubmit}>
+          <input
+            id="name"
+            name="full name"
+            type="text"
+            value={name}
+            onChange={handleChange}
+            placeholder="full name"
+          />
+        </form>
+      )}
