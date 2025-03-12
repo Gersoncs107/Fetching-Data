5a1af94aa49b2fae27601b694c39b5ca1a0bf158
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-11 23:24:55 -0300

    react

--- src/useRef.jsx
+++ src/useRef.jsx
@@ -17,6 +17,15 @@ export default function FormComponent() {
       }
 
       return(
-        
+        <form onSubmit={handleSubmit}>
+          <input
+            id="name"
+            name="full name"
+            type="text"
+            value={name}
+            onChange={handleChange}
+            placeholder="full name"
+          />
+        </form>
       )
 }
